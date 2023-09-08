b86a9ad4cbadd06e2e9ebeb15dd1ad1f4ecfd684
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-08 14:27:42 +0200

    milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "created": "2021-06-25"
+        "created": "06-25-2021"
     },
     {
         "id": 2,
@@ -19,7 +19,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=10"
         },
         "likes": 120,
-        "created": "2021-09-03"
+        "created": "09-03-2021"
     },
     {
         "id": 3,
@@ -30,7 +30,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=20"
         },
         "likes": 78,
-        "created": "2021-05-15"
+        "created": "05-15-2021"
     },
     {
         "id": 4,
@@ -41,7 +41,7 @@ const posts = [
             "image": null
         },
         "likes": 56,
-        "created": "2021-04-03"
+        "created": "04-03-2021"
     },
     {
         "id": 5,
@@ -52,6 +52,58 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
-        "created": "2021-03-05"
+        "created": "03-05-2021"
     }
 ];
+const postsListEl = document.querySelector(".posts-list");
+
+/**
+ * Generate a post
+ * @param {string} authorImage 
+ * @param {string} authorName 
+ * @param {string} dateCreated 
+ * @param {string} contentText 
+ * @param {string} media 
+ * @param {number} idPost 
+ * @param {number} likes 
+ * @returns 
+ */
+function addPost (authorImage, authorName, dateCreated, contentText, media, idPost, likes){
+   return `<div class="post">
+      <div class="post__header">
+         <div class="post-meta">                    
+            <div class="post-meta__icon">
+               <img class="profile-pic" src=${authorImage} alt=${authorName}>                    
+            </div>
+            <div class="post-meta__data">
+               <div class="post-meta__author">${authorName}</div>
+               <div class="post-meta__time">${dateCreated}</div>
+            </div>                    
+         </div>
+      </div>
+      <div class="post__text">
+         ${contentText}
+      </div>
+      <div class="post__image">
+         <img src=${media} alt="">
+      </div>
+      <div class="post__footer">
+         <div class="likes js-likes">
+            <div class="likes__cta">
+               <a class="like-button  js-like-button" href="#" data-postid=${idPost}>
+                  <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                  <span class="like-button__label">Mi Piace</span>
+               </a>
+            </div>
+            <div class="likes__counter">
+               Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+            </div>
+         </div> 
+      </div>            
+   </div>`
+};
+
+posts.forEach(post => postsListEl.insertAdjacentHTML("beforeend", addPost(post.author.image, post.author.name, post.created, post.content, post.media, post.id, post.likes)));
+
+
+
